f888176ad40157260823130cdff7d198099e2173
Sebastian <sebasortizsanta@gmail.com>
2025-11-29 16:51:38 -0300

    Sebas: [add] Using asynchronous code

--- WebApiStore/Controllers/ProductsController.cs
+++ WebApiStore/Controllers/ProductsController.cs
@@ -1,23 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiStore.Data;
 using WebApiStore.Entities;
 
 namespace WebApiStore.Controllers;
 
-[Route("api/[controller]")] // http://localhost:5002/api/products
+[Route("api/[controller]")] // https://localhost:5002/api/products
 [ApiController]
 public class ProductsController(StoreContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<List<Product>> GetProducts()
+    public async Task<ActionResult<List<Product>>> GetProducts()
     {
-        return context.Products.ToList();
+        return await context.Products.ToListAsync();
     }
 
-    [HttpGet("{id}")] // http://localhost:5002/api/products/3
-    public ActionResult<Product> GetProduct(int id)
+    [HttpGet("{id}")] // https://localhost:5002/api/products/3
+    public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = context.Products.Find(id);
+        var product = await context.Products.FindAsync(id);
         return product != null ? product : NotFound();
     }
 }
