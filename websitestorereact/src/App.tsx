cb8bde83e8d6085a75a6709c1fd13fd40b60824f
Sebastian <sebasortizsanta@gmail.com>
2025-11-30 09:03:17 -0300

    Sebas: [add] Using React state hook

--- websitestorereact/src/App.tsx
+++ websitestorereact/src/App.tsx
@@ -1,24 +1,33 @@
-const products = [
-  { name: 'product1', price: 100 },
-  { name: 'product2', price: 200 },
-  { name: 'product3', price: 300 },
-  { name: 'product3', price: 300 },
-  { name: 'product3', price: 300 },
-]
+import {useState} from "react";
 
 function App() {
-  return (
-    <div style={{fontSize: '1.6rem'}}>
-      <h1 style={{color: 'red'}}>Re-store</h1>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.name} - ${product.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    const [products, setProducts] = useState([
+        { name: 'product1', price: 100 },
+        { name: 'product2', price: 200 },
+        { name: 'product3', price: 300 },
+    ]);
+
+    const addProducts = () => {
+        setProducts(prevState => [...prevState,
+            { 
+                name: 'product' + (prevState.length + 1),
+                price: (prevState.length * 100) + 100
+            }]);
+    }
+    
+    return (
+        <div style={{fontSize: '1.6rem'}}>
+            <h1 style={{color: 'red'}}>Re-store</h1>
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>
+                        {product.name} - ${product.price}
+                    </li>
+                ))}
+            </ul>
+            <button onClick={addProducts}>Add Product</button>
+        </div>
+    )
 }
 
 export default App
