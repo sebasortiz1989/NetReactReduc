8b1bec78c8bf4275d655da5b56dd5898e1de77d2
Sebastian <sebasortizsanta@gmail.com>
2025-12-13 10:34:26 -0300

    Sebas: [add] Adding controllers to test errors

---
+++ WebApiStore/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiStore.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("not-found")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+    
+    [HttpGet("bad-request")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("This is a good request");
+    }
+
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("validation-error")]
+    public IActionResult GetValidationError()
+    {
+        ModelState.AddModelError("Problem1", "This is the first error");
+        ModelState.AddModelError("Problem2", "This is the second error");
+        return ValidationProblem();
+    }
+
+    [HttpGet("server-error")]
+    public IActionResult GetServerError()
+    {
+        throw new Exception("This is a server error");
+    }
+}
