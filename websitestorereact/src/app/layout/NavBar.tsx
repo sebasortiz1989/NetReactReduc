a815324ab4e6b478a1dc3e4c71c4958b48c6c767
Sebastian <sebasortizsanta@gmail.com>
2025-12-29 21:16:37 -0300

    Sebas: [add] Adding a user menu

--- websitestorereact/src/app/layout/NavBar.tsx
+++ websitestorereact/src/app/layout/NavBar.tsx
@@ -4,6 +4,7 @@ import {Link, NavLink} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../store/store.ts";
 import {setDarkMode} from "./uiSlice.ts";
 import {useFetchBasketQuery} from "../../features/basket/basketApi.ts";
+import UserMenu from "./UserMenu.tsx";
 
 const midLinks = [
     {title: 'catalog', path: '/catalog'},
@@ -29,6 +30,8 @@ const rightLinks = [
 ]
 
 export default function NavBar() {
+    const user = {email: 'seba@test.com', roles: []};
+    
     const { isLoading, darkMode } = useAppSelector(state => state.ui);
     const dispatch = useAppDispatch();
     const {data: basket} = useFetchBasketQuery();
@@ -63,13 +66,17 @@ export default function NavBar() {
                         </Badge>
                     </IconButton>
 
-                    <List sx={{display: 'flex'}}>
-                        {rightLinks.map(({title, path}) => (
-                            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
+                    {user ? (
+                        <UserMenu user={user} />
+                    ) : (
+                        <List sx={{display: 'flex'}}>
+                            {rightLinks.map(({title, path}) => (
+                                <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                        </List>
+                    )}
                 </Box>
             </Toolbar>
             {isLoading && (
