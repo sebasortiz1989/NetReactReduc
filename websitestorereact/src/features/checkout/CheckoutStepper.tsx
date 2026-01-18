3992164b08593e70008dab25be0b59fb124b567c
Sebastian <sebasortizsanta@gmail.com>
2026-01-18 11:32:28 -0300

    Sebas: [add] Updated address functionality

--- websitestorereact/src/features/checkout/CheckoutStepper.tsx
+++ websitestorereact/src/features/checkout/CheckoutStepper.tsx
@@ -1,25 +1,50 @@
 import {useState} from "react";
-import {Box, Button, FormControlLabel, Paper, Step, StepLabel, Stepper} from "@mui/material";
-import {AddressElement, PaymentElement} from "@stripe/react-stripe-js";
-import { CheckBox } from "@mui/icons-material";
+import {Box, Button, Checkbox, FormControlLabel, Paper, Step, StepLabel, Stepper, Typography} from "@mui/material";
+import {AddressElement, PaymentElement, useElements} from "@stripe/react-stripe-js";
 import Review from "./Review.tsx";
-import {useFetchAddressQuery} from "../account/accountApi.ts";
+import {useFetchAddressQuery, useUpdateUserAddressMutation} from "../account/accountApi.ts";
 import type {Address} from "../../app/models/User.ts";
 
 const steps = ['Address', 'Payment', 'Review'];
 
 export default function CheckoutStepper() {
     const [activeStep, setActiveStep] = useState(0);
-    const {data: {name, ...restAddress} = {} as Address} = useFetchAddressQuery();
-    
-    const handleNext = () => {
+    const {data: {name, ...restAddress} = {} as Address, isLoading} = useFetchAddressQuery();
+    const [updateAddress] = useUpdateUserAddressMutation();
+    const [saveAddressChecked, setSaveAddressChecked] = useState(false);
+    const elements = useElements();
+
+    const handleNext = async () => {
+        if (activeStep === 0 && saveAddressChecked && elements) {
+            const address = await getStripeAddress();
+            if (address) {
+                await updateAddress(address);
+            }
+        }
         setActiveStep(activeStep + 1);
     };
-    
+
     const handleBack = () => {
         if (activeStep === 0) return;
         setActiveStep(activeStep - 1);
     }
+
+    const getStripeAddress = async () => {
+        const addressElement = elements?.getElement('address');
+        if (!addressElement) {
+            return null;
+        }
+
+        const {value: {name, address}} = await addressElement.getValue();
+        if (name && address) {
+            return {...address, name};
+        }
+
+        return null;
+    }
+
+    if (isLoading)
+        return <Typography variant="h6">Loading Checkout...</Typography>;
     
     return (
         <Paper sx={{ p: 3, borderRadius: 3 }}>
@@ -32,7 +57,7 @@ export default function CheckoutStepper() {
                     );
                 })}
             </Stepper>
-            
+
             <Box sx={{mt: 2}}>
                 <Box sx={{display: activeStep === 0 ? 'block' : 'none'}}>
                     <AddressElement
@@ -46,7 +71,12 @@ export default function CheckoutStepper() {
                     />
                     <FormControlLabel
                         sx={{display: 'flex', justifyContent: 'end'}}
-                        control={<CheckBox />}
+                        control={
+                            <Checkbox
+                                checked={saveAddressChecked}
+                                onChange={(event) => setSaveAddressChecked(event.target.checked)}
+                            />
+                        }
                         label="Save as default address"
                     />
                 </Box>
@@ -57,7 +87,7 @@ export default function CheckoutStepper() {
                     <Review/>
                 </Box>
             </Box>
-            
+
             <Box display='flex' paddingTop={2} justifyContent='space-between'>
                 <Button onClick={handleBack}>
                     Back
