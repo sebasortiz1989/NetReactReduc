76b9dcce6837f93557e2856e0d18b7d2c23d7aa1
Sebastian <sebasortizsanta@gmail.com>
2026-02-07 17:51:12 -0300

    Sebas: [add] Creating a reusable Select input

--- websitestorereact/src/features/admin/InventoryPage.tsx
+++ websitestorereact/src/features/admin/InventoryPage.tsx
@@ -59,7 +59,7 @@ export default function InventoryPage() {
                         ))}
                     </TableBody>
                 </Table>
-                <Box>
+                <Box sx={{p:3}}>
                     {data && data.pagination && data.items.length > 0 && (
                         <AppPagination
                             metadata={data.pagination}
--- websitestorereact/src/features/admin/ProductForm.tsx
+++ websitestorereact/src/features/admin/ProductForm.tsx
@@ -3,12 +3,16 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {Box, Button, Grid, Paper, Typography} from "@mui/material";
 import {useForm} from "react-hook-form";
 import AppTextInput from "../../app/shared/components/AppTextInput.tsx";
+import {useFetchFiltersQuery} from "../catalog/catalogApi.ts";
+import AppSelectInput from "../../app/shared/components/AppSelectInput.tsx";
 
 export default function ProductForm() {
     const { control, handleSubmit } = useForm({
-        // mode: 'onTouched',
+        mode: 'onTouched',
         resolver: zodResolver(createProductSchema),
     });
+    
+    const {data} = useFetchFiltersQuery();
   
     const onSubmit = (data: CreateProductSchema) => {
         console.log(data);
@@ -23,6 +27,39 @@ export default function ProductForm() {
                 <Grid container spacing={3}>
                     <Grid size={12}>
                         <AppTextInput control={control} label="Product Name" name="name"/>
+                    </Grid>
+                    <Grid size={6}>
+                        {data?.brands &&
+                            <AppSelectInput
+                                items={data.brands}
+                                control={control}
+                                label="Brand"
+                                name="brand"/>}
+                    </Grid>
+                    <Grid size={6}>
+                        {data?.types &&
+                            <AppSelectInput
+                                items={data.types}
+                                control={control}
+                                label="Type"
+                                name="type"/>}
+                    </Grid>
+                    <Grid size={6}>
+                        <AppTextInput type={"number"} control={control} label="Price in cents" name="price"/>
+                    </Grid>
+                    <Grid size={6}>
+                        <AppTextInput type={"number"} control={control} label="Quantity in stock" name="quantity"/>
+                    </Grid>
+                    <Grid size={12}>
+                        <AppTextInput
+                            control={control}
+                            multiline
+                            rows={4}
+                            label="Description"
+                            name="description"/>
+                    </Grid>
+                    <Grid size={12}>
+                        <AppTextInput control={control} label="Image" name="file"/>
                     </Grid>
                 </Grid>
                 <Box display="flex" justifyContent="space-between" sx={{mt: 3}}>
